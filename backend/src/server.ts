d5439c4831ebb5008579bc5761666c67496ad30a
Leonardo <34871458+LeozinH1@users.noreply.github.com>
2022-05-20 02:05:07 -0300

    Funções separadas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,30 +1,4 @@
-import cors from "cors";
-import express, { Request, Response, NextFunction } from "express";
-import routes from "./routes";
-import "express-async-errors";
-import AppError from "./errors/AppError";
-import "./database";
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-app.use(routes);
-
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response
-      .status(err.statusCode)
-      .json({ status: "error", message: err.message });
-  }
-
-  console.error(err);
-
-  return response
-    .status(500)
-    .json({ status: "error", message: "Internal server error" });
-});
+import app from "./app";
 
 app.listen(3333, () => {
   console.log(`🚀 HTTP Server running on port 3333`);
